7aeabcd093fcc25cc5e9c0b7a2b65038dbc9a932
Marcin Koz³owski <marcin.kozlowski@dgt.eu>
2021-09-29 00:29:28 +0200

    Add project files.

---
+++ EF_DEMO/DataContext/AppDbContext.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using EF_DEMO.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace EF_DEMO.DataContext
+{
+    public partial class AppDbContext : DbContext
+    {
+        public AppDbContext()
+        {
+        }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public virtual DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseMySql("Server=127.0.0.1;Database=test;User=root;Password=sosNa;TreatTinyAsBoolean=true;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.ToTable("users");
+
+                entity.HasKey(e => e.Id);
+
+                entity.Property(e => e.Id)
+                    .HasColumnName("ID")
+                    .HasColumnType("int(11)");
+
+                entity.Property(e => e.UserName)
+                    .HasColumnName("USER_NAME")
+                    .HasColumnType("varchar(50)");
+            });
+        }
+    }
+}
---
+++ EF_DEMO/Program.cs
@@ -0,0 +1,37 @@
+﻿using EF_DEMO.DataContext;
+using EF_DEMO.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace EF_DEMO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Add new item?");
+            Console.ReadKey();
+            var context = new AppDbContext();
+            var user = new User()
+            {
+                UserName = "Gacek2"
+            };
+            context.Users.Add(user);
+            context.SaveChanges();
+            Console.WriteLine("New item added");
+            Console.ReadKey();
+        }
+
+        public static void ConfigureServices(IServiceCollection services)
+        {
+            var connectionString = "server=127.0.0.1;user=root;password=sosNa;database=test";
+
+            services.AddDbContext<AppDbContext>(
+                dbContextOptions => dbContextOptions
+                    .UseMySql(connectionString)
+                    .EnableSensitiveDataLogging()
+            );
+        }
+    }
+}
